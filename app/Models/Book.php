55f831e5689002c8f0596a9f4d2045990d21c9af
pinof <fattoruso.g@gmail.com>
2022-03-02 23:05:33 +0100

    create table model and controller books

---
+++ app/Models/Book.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    protected $table = 'books';
+    protected $fillable = [
+                            'title',
+                            'isbn',
+                            'year_edition',
+                            'author_id',
+                            'kind_id'
+                        ];
+
+
+}
