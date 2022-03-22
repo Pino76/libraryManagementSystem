4517035a90e512b336cdaf02880915b100b9351d
pinof <fattoruso.g@gmail.com>
2022-03-22 22:16:55 +0100

    refactor migration

--- database/migrations/2022_03_13_204057_create_status_books_table.php
+++ database/migrations/2022_03_13_204057_create_status_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBooksTable extends Migration
+class CreateStatusBooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateBooksTable extends Migration
      */
     public function up()
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('status_books', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('isbn');
-            $table->integer('year_edition');
-            $table->integer('author_id');
-            $table->integer('kind_id');
+            $table->foreignId('book_id')->constrained('books')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->date('start_date');
+            $table->date('end_date');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateBooksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('status_books');
     }
 }
